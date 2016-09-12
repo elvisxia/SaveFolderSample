49927fabbed6d8a0475a74586d02525ae7a27be0
v-xucxia <v-xucxia@PC-WINFFEE>
2016-09-12 12:15:03 +0800

    Add project files.

---
+++ SaveFolderSample/MainPage.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace SaveFolderSample
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void myBtn_Click(object sender, RoutedEventArgs e)
+        {
+            var picker=new  Windows.Storage.Pickers.FolderPicker();
+            picker.SuggestedStartLocation = Windows.Storage.Pickers.PickerLocationId.Desktop;
+            picker.FileTypeFilter.Add(".exe");//add any extension to avoid exception
+            var folder=await picker.PickSingleFolderAsync();
+            if (folder != null)
+            {
+                Windows.Storage.AccessCache.StorageApplicationPermissions.FutureAccessList.AddOrReplace("PickedFolderToken", folder);
+                var myNewFolder=await folder.CreateFolderAsync("myNewFolder");
+            }
+        }
+
+        private async void myBtn2_Click(object sender, RoutedEventArgs e)
+        {
+            var folder = await Windows.Storage.AccessCache.StorageApplicationPermissions.FutureAccessList.GetFolderAsync("PickedFolderToken");
+            if (folder != null)
+            {
+                var newFolder=folder.CreateFolderAsync("myQuietFolder");
+            }
+        }
+    }
+}
